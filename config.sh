ab694c0d85cd160e3b2ee9caef4fa98d392fa0db
Abdul Rahiman AT <31490398+abdulrahimanat@users.noreply.github.com>
2020-11-24 19:54:15 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-set -euox pipefail
-
+#set -euox pipefail
+#set -x
 #Variables
-ROLE_NAME=test-configwe
-POLICY_NAME=my-policys
-S3_BUCKET=prince-hellogdsff
-SNS_TOPIC=test-topics
+ROLE_NAME=test-confide
+POLICY_NAME=my-polidw
+S3_BUCKET=hellogdsffffdfhsfb
+SNS_TOPIC=test-topicssdq
+AWS_REGION=eu-central-1
+includeGlobalResourceTypes=false #true or false whether or not Global services to be enabled or not
+
 
 TRUST_POLICY='{
   "Version": "2012-10-17",
@@ -57,19 +60,18 @@ policy_attach=$(aws iam create-policy --policy-name "$POLICY_NAME" --policy-docu
 }
 # Attach custom Policy to Config service
 attach_custom_config_policy() {
-policy_attach=$(aws iam attach-role-policy --role-name "$ROLE_NAME" --policy-arn arn:aws:iam::327557370220:policy/${POLICY_NAME})
+policy_attach=$(aws iam attach-role-policy --role-name "$ROLE_NAME" --policy-arn arn:aws:iam::211539041645:policy/${POLICY_NAME})
 }
-
 
 
 # Create Bucket
 create_s3_bucket() {
-aws s3 mb s3://"${S3_BUCKET}"
+aws s3 mb s3://"${S3_BUCKET}" --region $AWS_REGION
 }
 
 # Create SNS
 create_sns_topic() {
-topic_arn=$(aws sns create-topic --name $SNS_TOPIC --output text)
+topic_arn=$(aws sns create-topic --name $SNS_TOPIC --region $AWS_REGION --output text)
 echo "Topic ARN: $topic_arn"
 }
 
@@ -79,15 +81,38 @@ echo "Topic ARN: $topic_arn"
 
 
 #Enabling AWS config
-enable_aws_config() {
-aws configservice subscribe --s3-bucket ${S3_BUCKET} --sns-topic $topic_arn --iam-role $ARN
+#enable_aws_config() {
+#aws configservice subscribe --s3-bucket ${S3_BUCKET} --sns-topic $topic_arn --iam-role $ARN
+#}
+
+put_config() {
+put_config=$(aws configservice put-configuration-recorder --configuration-recorder name=default,roleARN=$ARN --recording-group allSupported=true,includeGlobalResourceTypes=$includeGlobalResourceTypes --region $AWS_REGION)
 }
+
+create_sns_topic
+
+
+cat <<EOF >./delivery.json
+"name": "default",
+"s3BucketName": "${S3_BUCKET}",
+"snsTopicARN": "${topic_arn}",
+"configSnapshotDeliveryProperties": {
+"deliveryFrequency": "TwentyFour_Hours"
+}
+}
+EOF
+
+enable_aws_config() {
+put_delivery=$(aws configservice put-delivery-channel --delivery-channel file://./delivery.json --region $AWS_REGION) 
+enable_aws_config=$(aws configservice start-configuration-recorder --configuration-recorder-name default --region $AWS_REGION) 
+}
+
 
 create_iam_role
 attach_config_policy
 create_custom_config_policy
 attach_custom_config_policy
-create_sns_topic
 create_s3_bucket
 sleep 10s
+put_config
 enable_aws_config
